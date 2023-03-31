ed6b25541fe05f6f571a867f06b07882af7180e5
Trong Ho <trong.ho@vexere.com>
2023-03-31 16:56:25 +0700

    add build CI

--- App.tsx
+++ App.tsx
@@ -24,7 +24,6 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import { WebView } from 'react-native-webview';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -63,39 +62,39 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  return <WebView source={{uri: 'https://chat.openai.com/chat'}} />;
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits. Changed
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
-
-//  {/* <SafeAreaView style={backgroundStyle}>
-//         <StatusBar
-//           barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//           backgroundColor={backgroundStyle.backgroundColor}
-//         />
-//         <ScrollView
-//           contentInsetAdjustmentBehavior="automatic"
-//           style={backgroundStyle}>
-//           <Header />
-//           <View
-//             style={{
-//               backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//             }}>
-//             <Section title="Step One">
-//               Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-//               screen and then come back to see your edits. Changed
-//             </Section>
-//             <Section title="See Your Changes">
-//               <ReloadInstructions />
-//             </Section>
-//             <Section title="Debug">
-//               <DebugInstructions />
-//             </Section>
-//             <Section title="Learn More">
-//               Read the docs to discover what to do next:
-//             </Section>
-//             <LearnMoreLinks />
-//           </View>
-//         </ScrollView>
-//       </SafeAreaView> */}
 
 const styles = StyleSheet.create({
   sectionContainer: {
